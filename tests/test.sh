e61879646e8ad1bccc0cefccd879e96915e3254a
Maarten van Gompel <proycon@anaproy.nl>
2019-10-19 14:26:45 +0200

    added testing function

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # Should run in a Python virtual environment ideally
+bold=$(tput bold)
+boldred=${bold}$(tput setaf 1) #  red
+boldgreen=${bold}$(tput setaf 2) #  green
+green=${normal}$(tput setaf 2) #  green
+yellow=${normal}$(tput setaf 3) #  yellow
+blue=${normal}$(tput setaf 4) #  blue
+boldblue=${bold}$(tput setaf 4) #  blue
+normal=$(tput sgr0)
 
 TESTDIR=`dirname $0`
 cd $TESTDIR
@@ -9,13 +17,17 @@ HOSTNAME=$(hostname)
 
 echo "Checking installation..." >&2
 
-GOOD=1
+ALLGOOD=1 #falsify
 
 which clamservice 2>/dev/null
 if [ $? -ne 0 ]; then
    echo "ERROR: clamservice not found" >&2
    exit 2
 fi
+
+#kill any existing webservices
+kill $(ps aux | grep 'piereling' | awk '{print $2}') 2>/dev/null
+sleep 2
 
 echo "Starting piereling webservice" >&2
 clamservice -d piereling.piereling 2> servicetest.server.log &
@@ -24,38 +36,67 @@ sleep 10
 
 cd tests
 
-echo "Testing txt2folia..." >&2
-if ! curl -v -X PUT http://$HOSTNAME:8080/txt2folia; then
-    echo "  ERROR: unable to create project">&2
-    GOOD=0
-fi
-if ! curl -v -F "inputtemplate=txt2folia_in" -F "file=@test.txt" http://$HOSTNAME:8080/txt2folia/input/test.txt; then
-    echo "  ERROR: unable to upload input file">&2
-    GOOD=0
-fi
-if ! curl -v -X POST http://$HOSTNAME:8080/txt2folia/; then
-    echo "  ERROR: unable to start project">&2
-    GOOD=0
-else
-    sleep 5 #five seconds should hopefully be enough for processing (saves more complex polling)
-    mkdir output
-    if ! curl -v http://$HOSTNAME:8080/txt2folia/output/test.folia.xml; then
-        echo "  ERROR: unable to get output">&2
-        GOOD=0
+function test2folia() {
+    echo "${boldblue}Testing $1...${normal}" >&2
+    echo "----------------------------------------------" >&2
+    local OK=1
+    if ! curl -v -X PUT http://$HOSTNAME:8080/$1; then
+        echo "  ${boldred}ERROR: unable to create project${normal}">&2
+        OK=0
     fi
-    cd ..
-    rm -Rf output
-    #delete the project so we don't pollute the server
-    curl -v -X DELETE http://$HOSTNAME:8080/txt2folia
-fi
+    if ! curl -v -F "inputtemplate=${1}_in" -F "file=@$2" http://$HOSTNAME:8080/$1/input/$2; then
+        echo "  ${boldred}ERROR: unable to upload input file${normal}">&2
+        OK=0
+    fi
+    if ! curl -v -X POST http://$HOSTNAME:8080/$1/; then
+        echo "  ${boldred}ERROR: unable to start project${normal}">&2
+        OK=0
+    else
+        if [ -z "$4" ]; then
+            sleep 5 #five seconds should hopefully be enough for processing (saves more complex polling)
+        else
+            sleep $4
+        fi
+        mkdir output
+        cd output
+        if ! curl -v http://$HOSTNAME:8080/$1/output/$3 > $3; then
+            echo "  ${boldred}ERROR: unable to get output${normal}">&2
+            OK=0
+        else
+            cat $3
+            if ! foliavalidator $3; then
+                echo "  ${boldred}ERROR: folia output failed to validate${normal}">&2
+                OK=0
+            fi
+        fi
+        cd ..
+        rm -Rf output
+        #delete the project so we don't pollute the server
+        curl -v -X DELETE http://$HOSTNAME:8080/$1
+        if [ $OK -eq 1 ]; then
+            echo "${boldgreen}Done testing $1: SUCCESS!${normal}"
+        else
+            echo "${boldred}Done testing $1: FAILURE!${normal}"
+            ALLGOOD=0
+        fi
+    fi
+}
 
 
+test2folia txt2folia test.txt test.folia.xml
+test2folia txt2folia test2.txt test2.folia.xml
+test2folia rst2folia test.rst test.folia.xml
 
-if [ $GOOD -eq 1 ]; then
-    echo "Done, all tests passed!" >&2
+echo "Stopping piereling service" >&2
+kill $(ps aux | grep 'piereling' | awk '{print $2}') 2>/dev/null
+sleep 2
+
+
+if [ $ALLGOOD -eq 1 ]; then
+    echo "${boldgreen}Done, all tests passed!${normal}" >&2
     exit 0
 else
-    echo "TESTS FAILED!!!!" >&2
+    echo "${boldred}TESTS FAILED!!!!${normal}" >&2
     exit 1
 fi
 
